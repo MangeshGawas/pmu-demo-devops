871dd3607264a49e1bc7ed8467b7d1266d71e73b
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-11-04 15:45:28 +0530

    added frontend component
    - added the  3 component for UI purpose
    - added yml script for automatic build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AttendanceListComponent } from './components/attendance-list/attendance-list.component'; 
+import { MarkAttendanceComponent } from './components/mark-attendance/mark-attendance.component'; 
+import { DashboardComponent } from './components/dashboard/dashboard.component'; 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'attendance-list', component: AttendanceListComponent },
+  { path: 'mark-attendance', component: MarkAttendanceComponent },
+  { path: '', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AttendanceListComponent } from './components/attendance-list/attendance-list.component';
+import { MarkAttendanceComponent } from './components/mark-attendance/mark-attendance.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttendanceListComponent,
+    MarkAttendanceComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
